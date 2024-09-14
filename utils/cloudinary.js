138bc3ffce5c5db3e1f40566ead0d186729f33a6
MACAULAY ISRAEL <ayomideizzi@gmail.com>
2024-09-14 19:43:34 +0100

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
-const config = require("./config");
+const config = require("/opt/render/project/src/config");
 
 // Configure Cloudinary with your credentials
 cloudinary.config({
