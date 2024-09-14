8e873affb9aed7619be88ddc28f00c3e23c83b5f
MACAULAY ISRAEL <ayomideizzi@gmail.com>
2024-09-14 19:46:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+const app = require("/opt/render/project/src/app");
+const config = require("/opt/render/project/src/utils/config");
+const logger = require("/opt/render/project/src/utils/logger");
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
